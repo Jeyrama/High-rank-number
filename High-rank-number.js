5ee6720139162f80f4e97b4afc631cc7cf26daf4
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-13 20:45:31 -0800

    Update High-rank-number.js

--- High-rank-number.js
+++ High-rank-number.js
@@ -12,3 +12,8 @@ Examples:
 
 
 // Solution
+
+function highestRank(arr) {
+  let getNum = num => arr.filter(n => n === num).length;
+  return arr.sort((a,b) => getNum(b) - getNum(a) || b - a)[0];
+}
